796e119c95b71429ca9ede777c83a1415ccf6343
Rahul Patil <55730676+rahulp34@users.noreply.github.com>
2022-12-27 12:56:19 +0530

    commited changes

--- raaghu-elements/src/rds-scrollspy/src/lib/scrollspy.stories.ts
+++ raaghu-elements/src/rds-scrollspy/src/lib/scrollspy.stories.ts
@@ -53,7 +53,6 @@ template: `
   <scrollspy-item scrollItemId="scrollspyHeading1" header = "First header">
     <p>This is some placeholder content for the scrollspy page. Note that as you scroll down the page, the appropriate navigation link is highlighted. It's repeated throughout the component example. We keep adding some more example copy here to emphasize the scrolling and highlighting</p>
     <div class="d-flex justify-content-end">
-    <a href="#scrollspy" target="_self" class="m-1 text-decoration-none">Go Top</a>
     </div>
   </scrollspy-item>
   </div>
@@ -61,7 +60,6 @@ template: `
   <scrollspy-item scrollItemId="scrollspyHeading2" header = "Second header">
   <p>This is some placeholder content for the scrollspy page. Note that as you scroll down the page, the appropriate navigation link is highlighted. It's repeated throughout the component example. We keep adding some more example copy here to emphasize the scrolling and highlighting</p>
   <div class="d-flex justify-content-end">
-  <a href="#scrollspy" target="_self" class="m-1 text-decoration-none">Go Top</a>
   </div>
   </scrollspy-item>
   </div>
@@ -69,7 +67,6 @@ template: `
   <scrollspy-item scrollItemId="scrollspyHeading3" header = "Third header">
   <p>This is some placeholder content for the scrollspy page. Note that as you scroll down the page, the appropriate navigation link is highlighted. It's repeated throughout the component example. We keep adding some more example copy here to emphasize the scrolling and highlighting</p>
   <div class="d-flex justify-content-end">
-  <a href="#scrollspy" target="_self" class="m-1 text-decoration-none">Go Top</a>
   </div>
   </scrollspy-item>
   </div>
