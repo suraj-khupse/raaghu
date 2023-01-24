60b8c81befc2d2a7cee5e815c1478df881a4c156
Bhorga <harshit.bhorga@waiin.com>
2023-01-24 15:18:04 +0530

    sync-elements updated

--- raaghu-mfe/utils/sync-elements.js
+++ raaghu-mfe/utils/sync-elements.js
@@ -10,7 +10,7 @@ const filesToReplace = [
     '.gitignore',
     'tsconfig.json'
 ];
-const dependentElements = [
+const dependentElements = [   
     'rds-label',
     'rds-toggle',
     'rds-icon-label',
@@ -51,8 +51,7 @@ function mergeTSConfigJson() {
             ]
         };
         changesDone = true;
-    };
-    
+    };   
     if (ngElementsFile.compilerOptions.paths["@libs/rds-toggle"] == undefined) {
         ngElementsFile.compilerOptions.paths = {
             ...ngElementsFile.compilerOptions.paths,
@@ -295,7 +294,7 @@ function buildDependentElements() {
     console.log('Building dependent \x1b[32m' + dependentElements.toString() + '\x1b[0m elements...');
     // let commandline = 'concurrently ';
     for (const element of dependentElements) {
-        execSync(`npm run build ${element} > output.log &`, { cwd: ngElementsDir, stdio: 'inherit' });
+        execSync(`npm run build ${element}`, { cwd: ngElementsDir, stdio: 'inherit' });
         // commandline = commandline + ' \"npm run build ' + element + '\"';
     }
     // execSync(`${commandline}`, { cwd: ngElementsDir, stdio: 'inherit' });
