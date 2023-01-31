e390a30af08aac5424dc42dfc34de3acea66d236
bhorga <harshit.bhorga@waiin.com>
2023-01-31 15:26:46 +0530

    language-text page

--- raaghu-mfe/projects/rds-page-language-text/src/app/app.module.ts
+++ raaghu-mfe/projects/rds-page-language-text/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EditLanguageTextComponent } from './edit-language-text/edit-language-text.component';
+import { RdsCompDataTableModule } from 'projects/rds-components/src/app/rds-comp-data-table/rds-comp-data-table.module';
 
 @NgModule({
   declarations: [
@@ -16,6 +17,7 @@ import { EditLanguageTextComponent } from './edit-language-text/edit-language-te
     BrowserModule,
     AppRoutingModule,SharedModule,
     RdsOffcanvasModule,
+    RdsCompDataTableModule,
     NgxTranslateModule.forRoot(),
     RdsButtonModule,RdsTextareaModule,FormsModule,RdsSelectListModule,RdsSearchInputModule
   ],
